d3ec2bd01d0603d3dab39ffc491f3cc095f53ecb
hojkimg77 <rlaghwnd4534@gmail.com>
2025-12-30 12:54:36 +0900

    fix: etc

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import { useWeb3StatusStore } from "@/src/store/web3StatusStore";
+import { useEffect } from "react";
 
 export default function MainPage() {
   const { metamaskInstalled } = useWeb3StatusStore();
 
+  useEffect(() => {
+    if (!metamaskInstalled) {
+      alert("please install metaMask");
+    }
+  }, [metamaskInstalled]);
+
   if (!metamaskInstalled) {
-    alert("please install metaMask");
     return <div className="text-red-500">metaMask is not installed</div>;
   }
 
