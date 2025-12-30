68ad84b395826449375ebb278be9ddf06de5ee75
hojkimg77 <rlaghwnd4534@gmail.com>
2025-12-30 11:58:51 +0900

    fix:remove suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import { Suspense } from "react";
 import Web3Initializer from "../components/common/Web3Initializer";
 
 export default function RootPage() {
-  return (
-    <Suspense fallback={null}>
-      <Web3Initializer />
-    </Suspense>
-  );
+  return <Web3Initializer />;
 }
